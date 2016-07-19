431a516be0f2cc4792a9836a24a07b28d789c1c3
lewrang <cyzzzowna@gmail.com>
2016-07-19 17:36:46 +0000

    new branch, assertion added

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ describe('Array', function() {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
       assert.equal(0, [1,2,3].indexOf(1));
+      assert.equal(1, [1,2,3].indexOf(2));
     });
   });
 });
